e722bffac4bcd3f59449b8766ca045b057c9993f
Garret Alfert <alfert@wevelop.de>
2022-09-12 14:07:05 +0200

    Use vercel/ncc

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 try {
-  const token = core.getInput("token", { required: true });
-  const databaseId = core.getInput("database-id", { required: true });
+  const token = core.getInput('token', { required: true });
+  const databaseId = core.getInput('database-id', { required: true });
 
-  console.log("Token", token);
-  console.log("Database ID", databaseId);
+  console.log('Token', token);
+  console.log('Database ID', databaseId);
 
   const context = github.context;
 
-  console.log("GitHub context", JSON.stringify(context, null, 2));
+  console.log('GitHub context', JSON.stringify(context, null, 2));
 } catch (error) {
   core.setFailed(error.message);
 }
